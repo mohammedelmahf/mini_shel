01c3c8f24d818baa095ebec6a88067db818ba994
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-07 20:45:07 +0100

    maelmahf : start parsing

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:02:17 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/07 20:06:32 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/07 20:45:04 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int main(int ac , char **av  , char **env)
     init_minishell(env);
     while(1)
     {
+        
         
     }
     start_execution();
