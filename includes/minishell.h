d1e439ffd7cddffa05a4a6a0649c9763ab9815e9
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-07 15:01:07 +0100

    maelmahf : start parsing

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:04:20 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/07 11:43:31 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/07 15:00:30 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,45 +18,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/types.h>
 
-
-extern int	g_last_exit_code;
-
-typedef struct s_token
-{
-
-}       t_token;
-
-typedef struct s_command
-{
-	char				*command;
-}	t_command;
-
-typedef struct s_data
-{
-    bool        interactive;
-    char        **env;
-    t_token     *token;
-    char        *user_input;
-    char		**env;
-	char		*working_dir;
-	char		*old_working_dir;
-    t_command   *cmd;
-    t_pid       pid;
-    
-}       t_data;
-//utilss
-size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *src);
-void	ft_putstr_fd(char *s, int fd);
-void	*ft_calloc(size_t count, size_t size);
-//utils
-bool    usage_message(bool return_valu);
-int error_msg_cmd(char *cmd , char *detail , char *error_message , int error_nb);
-bool    init_data(t_data *data , char **env);
-void    free_ptr(void *ptr);
-
-//env
-int env_var_count(char **env);
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -6,34 +6,9 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:02:17 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/07 11:29:38 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/07 15:00:36 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"../includes/minishell.h"
 
-int	g_last_exit_code;
-
-static bool start_check(t_data *data, int ac , char **av)
-{
-    if(ac != 1 && ac != 3)
-        return (usage_message(false));
-    if(ac == 3)
-    {
-        data->interactive = false;
-        if(!av[1] || (av[1] && ft_strcmp(av[1] , "-c") != 0))
-            return (usage_message(false));
-        if(!av[2] || (av[2] && av[2][0] == '\0'))
-            return (usage_message(false));
-    }
-    else
-        data->interactive = true;
-    return (true);
-}
-int main(int ac , char **av , char **env)
-{
-    t_data data;
-    
-    if(!start_check(&data , ac , av) || !init_data(&data , env))
-        exit_shell();
-}
