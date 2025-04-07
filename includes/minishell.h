2e22bdb01fa1715f4e051e7e5db661e7b4d91efe
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-07 15:13:00 +0100

    maelmahf : start parsing

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:04:20 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/07 15:00:30 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/07 15:11:11 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,37 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
+typedef struct	s_token
+{
+	char			*str;
+	int				type;
+	struct s_token	*prev;
+	struct s_token	*next;
+}				t_token;
 
+typedef struct	s_env
+{
+	char			*value;
+	struct s_env	*next;
+}				t_env;
+
+typedef struct	s_mini
+{
+	t_token			*start;
+	t_env			*env;
+	t_env			*secret_env;
+	int				in;
+	int				out;
+	int				fdin;
+	int				fdout;
+	int				pipin;
+	int				pipout;
+	int				pid;
+	int				charge;
+	int				parent;
+	int				last;
+	int				ret;
+	int				exit;
+	int				no_exec;
+}				t_mini;
 #endif
