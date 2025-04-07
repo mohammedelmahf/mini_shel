015e07d30dd2b9791879bef94dea4fbeedfd9d3a
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-07 17:17:40 +0100

    maelmahf : start parsing

---
+++ sources/utils/ft_lstadd_back_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 14:52:51 by maelmahf          #+#    #+#             */
+/*   Updated: 2025/04/07 17:17:34 by maelmahf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*ptr;
+
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst)
+	{
+		ptr = ft_lstlast(*lst);
+		ptr->next = new;
+	}
+	else
+		*lst = new;
+}
---
+++ sources/utils/ft_lstclear_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 16:03:21 by maelmahf          #+#    #+#             */
+/*   Updated: 2025/04/07 17:17:36 by maelmahf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*ptr;
+
+	if (lst && *lst && del)
+	{
+		while (*lst)
+		{
+			ptr = (*lst)->next;
+			ft_lstdelone(*lst, del);
+			*lst = ptr;
+		}
+	}
+}
