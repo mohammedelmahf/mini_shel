13a540bc89ad3ff0086b8ff0da436fbe42964cd0
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-08 11:59:06 +0100

    maelmahf

--- sources/env/env.c
+++ sources/env/env.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 15:28:33 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/08 10:46:42 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/08 11:57:49 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,15 @@ void    init_envlst(void)
     if(!environ)
         return ;
     i = 0;
+    // int j;
+    // j = 0;
     while(environ[i])
     {
+        // while(environ[j])
+        // {
+        //     printf("%s\n" , environ[j]);
+        //     j++;  
+        // }
         key = extract_key(environ[i]);
         value = extract_value(environ[i]);
         update_envlst(key, value, true);
