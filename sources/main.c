1763769a7f7aec58977700f9227951320ba3b79e
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-08 11:08:39 +0100

    maelmahf : create Makefile

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:02:17 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/08 10:58:05 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/08 11:08:11 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int main(int ac , char **av  , char **env)
     init_minishell(env);
     while(1)
     {
+        printf("here\n");
         init_signal();
         
     }
