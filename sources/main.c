1180c5b3b8a2e5a35f05cfc12dc11ed10454190c
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-09 22:15:19 +0100

    maelmahf

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:02:17 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/09 19:13:54 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/09 19:22:59 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,15 @@ int main(int ac , char **av  , char **env)
         if (!data.line)
             (/*clean_all(),*/ ft_putstr_fd("exit\n" , 1) , exit(data.exit_s));
         if(data.line[0])
-        add_history(data.line);  
+            add_history(data.line);
+                int i = 0; 
+        if(!ft_strncmp(data.line , "env" , 3))
+            while(env[i])
+            {
+                printf("%s\n" , env[i]);
+                i++;
+            }
+                
         data.tokens = tokenize();
             t_token *curr = data.tokens;
          while (curr)
