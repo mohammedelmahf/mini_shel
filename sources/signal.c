513b4fe535f1f55bf8b7b53c9262c8a2a6b1f9b6
Ilyas El Asefar <ilyass.42.1337@gmail.com>
2025-04-09 14:44:54 +0100

    iel-asf

--- sources/signal.c
+++ sources/signal.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-#include <termios.h>
 
 static void handler_sigint(int num)
 {
