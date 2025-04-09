7e745407f6a5192fca5e30bb680554d394d10276
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-09 10:31:11 +0100

    maelmahf

--- sources/tokenize/tokenization_handler.c
+++ sources/tokenize/tokenization_handler.c
@@ -6,11 +6,33 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 20:31:19 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/09 09:34:00 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/09 09:54:18 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int         handle_separator(char **line , char **token_list)
+{
+    if (!ft_strncmp(*line , ">>" , 2))
+        return(append_separator(T_DGREAT , line , token_list));
+    else if (!ft_strncmp(*line , "<<" , 2))
+        return(append_separator(T_DLESS , line , token_list));
+    else if (!ft_strncmp(*line , "<" , 1))
+        return(append_separator( T_LESS , line , token_list));
+    else if (!ft_strncmp(*line , ">" , 1))
+        return(append_separator(T_GREAT , line , token_list));
+    else if (!ft_strncmp(*line , "||" , 2))
+        return(append_separator( T_OR, line , token_list));
+    else if (!ft_strncmp(*line , "&&" , 2))
+        return(append_separator( T_AND, line , token_list));
+    else if (!ft_strncmp(*line , "(" , 1))
+        return(append_separator( T_O_PARENT, line , token_list));
+    else if (!ft_strncmp(*line , ")" , 1))
+        return(append_separator(T_C_PARENT , line , token_list));
+    else
+        return(append_separator(T_PIPE , line , token_list));
+}
 
 t_token    *tokenization_handler(char *line)
 {
@@ -23,6 +45,9 @@ t_token    *tokenization_handler(char *line)
     {
         if(error)
             return(clear_token_list(&token_list) , NULL);
-        if(!ft_strncmp)
+        if(!ft_strncmp(line , "<" , 1) || !ft_strncmp(line , ">" , 1)
+            || !ft_strncmp(line , "|" , 1) || !ft_strncmp(line , "&&" , 2)
+            || !ft_strncmp(line , "(" , 1) || !ft_strncmp(line , ")" , 1) )
+            error = (!handle_separator(&line , &token_list) && 1); 
     }
 }
