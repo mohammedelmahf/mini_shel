09941537269048118ec1a9847b3b014a244665cf
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-09 09:22:32 +0100

    maelmahf

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:02:17 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/08 20:53:14 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/08 22:18:41 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,9 @@ int main(int ac , char **av  , char **env)
 
      
             
-        data.tokens = tokenize();
-        if(!data.tokens)
-            continue;
+        //data.tokens = tokenize();
+        //if(!data.tokens)
+        //    continue;
         // printf("%s\n" ,data.line );
         // write(STDIN_FILENO, "hey", 4);
         
