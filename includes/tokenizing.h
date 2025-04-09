983e842f461d94daa3d4160a18e19ec88cb7e787
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-09 09:34:10 +0100

    maelmahf

--- includes/tokenizing.h
+++ includes/tokenizing.h
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 12:05:54 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/08 14:52:38 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/09 09:29:55 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,25 @@
 
 #define PROMPT "minishell> "
 
+typedef	enum	s_token_type
+{
+	T_IDENTIFIER,   // Command name or argument
+	T_LESS,         // <
+	T_GREAT,        // >
+	T_DLESS,        // <<
+	T_DGREAT,       // >>
+	T_PIPE,         // |
+	T_O_PARENT,     // (
+	T_C_PARENT,     // )
+	T_AND,          // &&
+	T_OR,           // ||
+	T_NL            // End of line (or newline)
+}				t_token_type;
+
 typedef struct	s_token
 {
-	char			*str;
-	int				type;
+	t_token_type			type;
+	char				*value;
 	struct s_token	*prev;
 	struct s_token	*next;
 }				t_token;
--- sources/tokenize/tokenization_handler.c
+++ sources/tokenize/tokenization_handler.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 20:31:19 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/09 09:24:10 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/09 09:34:00 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 t_token    *tokenization_handler(char *line)
 {
-    
+    int     error;
+    t_token *token_list;
+
+    error = 0;
+    token_list = NULL;
+    while(*line)
+    {
+        if(error)
+            return(clear_token_list(&token_list) , NULL);
+        if(!ft_strncmp)
+    }
 }
