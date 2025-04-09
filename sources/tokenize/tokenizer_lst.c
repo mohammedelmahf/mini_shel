87449d0b6b914cfc619cd5d3418e8c4d43516b84
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-09 11:36:53 +0100

    maelmahf

--- sources/tokenize/tokenizer_lst.c
+++ sources/tokenize/tokenizer_lst.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 11:13:27 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/09 11:31:04 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/09 11:36:48 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 t_token     *new_token(char *value , t_token_type type)
 {
-    t_token new_token
+    t_token *new_token;
+
+    new_token = (t_token *)ft_calloc(1 , sizeof(t_token));
+    if(!new_token)
+        return (NULL);
+    new_token->value = value;
+    new_token->type = type;
+    return (new_token);
 }
