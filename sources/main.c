f15aa19b574b7c39e54620f30ce02214ba6c1a46
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-14 09:27:54 +0100

    maelmahf

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:02:17 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/11 12:01:40 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/11 13:17:01 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int main(int ac , char **av  , char **env)
         }
         if(!data.tokens)
             continue;
-        //data.ast = start_parsing();
+        data.ast = start_parsing();
         
         //printf("%s\n" ,data.line );
         //write(STDIN_FILENO, "hey", 4);
