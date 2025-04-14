8d92016e09a91c4952c6c8f095f9ac8e38a07bbc
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-14 16:17:15 +0100

    malemahf

--- sources/parsing/parsing_help.c
+++ sources/parsing/parsing_help.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 13:54:47 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/14 16:15:23 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:16:19 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ t_node    *parse_single_cmd(void)
     {
         
     }
+    return (node);
 }
