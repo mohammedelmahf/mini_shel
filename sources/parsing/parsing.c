d6286614152936f6a32bf9e076ece5c2897c28b6
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-14 09:58:36 +0100

    maelmahf

--- sources/parsing/parsing.c
+++ sources/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 12:52:12 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/11 11:10:50 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/14 09:57:41 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ t_node  *terms()
 
     if(data.parse_error.type)
         return (NULL);
+    if(curr_token_type_is_binop() || data.curr_token->type = T_C_PARENT)
+        return (set_error(E_SYNTAX) , NULL);
 }
 t_node  *parsing_ast(int minimum_precedence)
 {
@@ -34,5 +36,5 @@ t_node  *start_parsing(void)
     t_node  *ast;
     
     data.curr_token = data.tokens;
-    ast = parsing_ast(0);
+    ast = parsing_ast(0); //expression
 }
