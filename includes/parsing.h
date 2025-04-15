b57d5fbd4aa024628bffaae451dc0db3d6837e41
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-15 09:14:48 +0100

    maelmahf

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 09:39:13 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/14 20:57:55 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/14 21:06:06 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,21 +46,21 @@ typedef struct s_parse_err
 
 }              t_parse_err;
 
-t_node  *start_parsing(void);
-void    set_error(t_parse_err_type type);
-void    set_error(t_parse_err_type type);
-bool    curr_token_type_is_binop(void);
-t_node    parse_single_cmd(void);
-t_node_type	get_node_type(t_token_type type);
-t_node	*lstnew(t_node_type type);
-void    next_token(void);
-int    curr_token_prec(void);
-int     prec(t_token_type type);
-bool	redirection(t_token_type type);
-t_node  *terms(void);
-t_node  *parsing_ast(int minimum_precedence);
-t_node      *join_nodes(t_token_type type , t_node *left , t_node *right);
-void    clear_ast(t_node **ast);
+t_node			*start_parsing(void);
+void			set_error(t_parse_err_type type);
+void			set_error(t_parse_err_type type);
+bool			curr_token_type_is_binop(void);
+t_node			parse_single_cmd(void);
+t_node_type		get_node_type(t_token_type type);
+t_node			*lstnew(t_node_type type);
+void			next_token(void);
+int				curr_token_prec(void);
+int				prec(t_token_type type);
+bool			redirection(t_token_type type);
+t_node			*terms(void);
+t_node			*parsing_ast(int minimum_precedence);
+t_node			*join_nodes(t_token_type type , t_node *left , t_node *right);
+void			clear_ast(t_node **ast);
 
 
 
