41be0ac1fa6529b116a47b55fffc9a981e558492
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-04-16 10:06:01 +0100

    maelmahf : Update parsing and parsing_help files with recent changes

--- sources/parsing/parsing.c
+++ sources/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 12:52:12 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/04/15 11:13:19 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/04/16 09:52:45 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ t_node  *terms(void)
     else
         printf("here\n");
     return (NULL);
-    // else
-    //     return (parse_single_cmd());
+    else
+        return (parse_single_cmd());
 }
 
 
