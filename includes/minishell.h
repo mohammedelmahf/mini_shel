63eb2f0c6bc095ddf6efefa811cec0c81631a062
Ilyas El Asefar <ilyass.42.1337@gmail.com>
2025-05-04 17:19:06 +0100

    iel-asef

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: iel-asef <iel-asef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 09:04:20 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/05/03 14:45:13 by iel-asef         ###   ########.fr       */
+/*   Updated: 2025/05/04 16:07:55 by iel-asef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -192,5 +192,9 @@ void    handler_sigquit(int num);
 void    clean_all(void);
 void execute_cmd(char **args, char **envp);
 
+void	free_tokens(t_token *tokens);
+void	free_ast(t_node *ast);
+
+ void	clean_cmd_data(void);
 
 #endif
