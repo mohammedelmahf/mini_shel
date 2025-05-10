d3a1dc5c9280a771095f66ec2202bcb7e1b4df64
Ilyas El Asefar <ilyass.42.1337@gmail.com>
2025-05-10 21:04:47 +0100

    iel-asef

--- sources/exec/error_msg.c
+++ sources/exec/error_msg.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_msg.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: iel-asef <iel-asef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 14:30:08 by iel-asef          #+#    #+#             */
-/*   Updated: 2025/05/10 16:27:39 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/05/10 20:45:36 by iel-asef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,8 @@ int	ft_err_msg(t_err err)
 	else if (err.msg == ERRMSG_NO_SUCH_FILE)
 		return (
 			ft_putstr_fd("minishell: ", 2),
-			ft_putstr_fd("No such file or directory", 2),
 			ft_putstr_fd(err.cause, 2),
-			ft_putstr_fd(": No such file or directory", 2),
+			ft_putstr_fd(": No such file or directory ", 2),
 			ft_putstr_fd("\n", 2),
 			err.num
 		);
@@ -35,7 +34,7 @@ int	ft_err_msg(t_err err)
 		return (
 			ft_putstr_fd("minishell: ", 2),
 			ft_putstr_fd(err.cause, 2),
-			ft_putstr_fd(": Permission denied", 2),
+			ft_putstr_fd(": Permission denied ", 2),
 			ft_putstr_fd("\n", 2),
 			err.num
 		);
@@ -43,12 +42,12 @@ int	ft_err_msg(t_err err)
 		return (
 			ft_putstr_fd("minishell: ", 2),
 			ft_putstr_fd(err.cause, 2),
-			ft_putstr_fd(": ambiguous redirect", 2),
+			ft_putstr_fd(": ambiguous redirect ", 2),
 			err.num
 		);
 	else if (err.msg == ERRMSG_TOO_MANY_ARGS)
 		return (
-			ft_putstr_fd("minishell: exit: too many arguments\n", 2),
+			ft_putstr_fd("minishell: exit: too many arguments \n", 2),
 			g_data.exit_s = 1,
 			err.num
 		);
@@ -56,7 +55,7 @@ int	ft_err_msg(t_err err)
 		return (
 			ft_putstr_fd("minishell: ", 2),
 			ft_putstr_fd(err.cause, 2),
-			ft_putstr_fd(": numeric argument required", 2),
+			ft_putstr_fd(": numeric argument required ", 2),
 			ft_putstr_fd("\n", 2),
 			err.num
 		);
