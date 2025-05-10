35a24c6cf410336e9d0f7a02146da11ccde3e816
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-05-10 16:23:20 +0100

    maelmahf

--- sources/expander/expanded.c
+++ sources/expander/expanded.c
@@ -6,94 +6,94 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 10:00:58 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/05/10 16:14:36 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/05/10 16:23:04 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char *handle_dollar(char *str, size_t *i)
-{
-    char *result = ft_strdup("");
-    char *tmp;
-
-    while (str[*i] == '$') {
-        (*i)++;
-        if (!str[*i]) {
-            tmp = ft_strdup("$");
-        }
-        else if (str[*i] == '$') {
-            (*i)++;
-            tmp = ft_itoa(getpid());
-        }
-        // $? → last exit status
-        else if (str[*i] == '?') {
-            (*i)++;
-            tmp = ft_itoa(g_data.exit_s);
-        }
-        // $1, $@, etc → unsupported
-        else if (ft_isdigit(str[*i]) || str[*i] == '@') {
-            (*i)++;
-            tmp = ft_strdup("");
-        }
-        // $ followed by invalid char (space, etc.) → literal $
-        else if (!is_valid_var_char(str[*i]))
-        {
-            if (str[*i] == '\0')
-                tmp = ft_strdup("");      // End of string after $
-            else if (str[*i] == '$')
-                tmp = ft_itoa(getpid());  // Handle $$ (already above, but safe here)
-            else
-                tmp = ft_strdup("");      // Invalid var character → expand to ""
-        }
-        // $VAR → lookup
-        else
-        {
-            size_t start = *i;
-            while (is_valid_var_char(str[*i]))
-                (*i)++;
-            char *var = ft_substr(str, start, *i - start);
-            char *val = get_envlst_value(var);
-            tmp = ft_strdup(val ? val : "");
-            free(var);
-        }
-
-        result = ft_strjoin_f(result, tmp);  // joins and frees old result
-    }
-
-    return result;
-}
-
-
-// char   *handle_dollar(char *str , size_t *i)
+// char *handle_dollar(char *str, size_t *i)
 // {
-//     size_t start;
-//     char *var;
-//     char *env_value;
+//     char *result = ft_strdup("");
+//     char *tmp;
+
+//     while (str[*i] == '$') {
+//         (*i)++;
+//         if (!str[*i]) {
+//             tmp = ft_strdup("$");
+//         }
+//         else if (str[*i] == '$') {
+//             (*i)++;
+//             tmp = ft_itoa(getpid());
+//         }
+//         // $? → last exit status
+//         else if (str[*i] == '?') {
+//             (*i)++;
+//             tmp = ft_itoa(g_data.exit_s);
+//         }
+//         // $1, $@, etc → unsupported
+//         else if (ft_isdigit(str[*i]) || str[*i] == '@') {
+//             (*i)++;
+//             tmp = ft_strdup("");
+//         }
+//         // $ followed by invalid char (space, etc.) → literal $
+//         else if (!is_valid_var_char(str[*i]))
+//         {
+//             if (str[*i] == '\0')
+//                 tmp = ft_strdup("");      // End of string after $
+//             else if (str[*i] == '$')
+//                 tmp = ft_itoa(getpid());  // Handle $$ (already above, but safe here)
+//             else
+//                 tmp = ft_strdup("");      // Invalid var character → expand to ""
+//         }
+//         // $VAR → lookup
+//         else
+//         {
+//             size_t start = *i;
+//             while (is_valid_var_char(str[*i]))
+//                 (*i)++;
+//             char *var = ft_substr(str, start, *i - start);
+//             char *val = get_envlst_value(var);
+//             tmp = ft_strdup(val ? val : "");
+//             free(var);
+//         }
+
+//         result = ft_strjoin_f(result, tmp);  // joins and frees old result
+//     }
+
+//     return result;
+// }
+
+
+char   *handle_dollar(char *str , size_t *i)
+{
+    size_t start;
+    char *var;
+    char *env_value;
 
     
-//     (*i)++;
-//     if(ft_isdigit(str[*i]) || str[*i] == '@')
-//     {
-//         (*i)++;
-//         return(ft_strdup(""));
-//     }
-//     else if(str[*i] == '?')
-//     {
-//         (*i)++;
-//         return (ft_itoa(g_data.exit_s));
-//     }
-//     else if(!is_valid_var_char(str[*i]))
-//          return(ft_strdup("$"));
-//     start = *i;
-//     while(is_valid_var_char(str[*i]))
-//         (*i)++;
-//     var = ft_substr(str , start , *i - start );
-//     env_value = get_envlst_value(var);
-//     if (!env_value)
-//         return (free(var), ft_strdup(""));
-//     return (free(var), ft_strdup(env_value)); 
-// }
+    (*i)++;
+    if(ft_isdigit(str[*i]) || str[*i] == '@')
+    {
+        (*i)++;
+        return(ft_strdup(""));
+    }
+    else if(str[*i] == '?')
+    {
+        (*i)++;
+        return (ft_itoa(g_data.exit_s));
+    }
+    else if(!is_valid_var_char(str[*i]))
+         return(ft_strdup("$"));
+    start = *i;
+    while(is_valid_var_char(str[*i]))
+        (*i)++;
+    var = ft_substr(str , start , *i - start );
+    env_value = get_envlst_value(var);
+    if (!env_value)
+        return (free(var), ft_strdup(""));
+    return (free(var), ft_strdup(env_value)); 
+}
 
 char    *cmd_pre_expand(char *str)
 {
